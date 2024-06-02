a76e353a867e60c73a76244622fbf3e20bf0b9f7
FFernandes4280 <d2022002088@unifei.edu.br>
2024-06-01 23:31:30 -0300

    Agora tá fluindo

---
+++ src/app/(autenticar)/_layout.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Redirect, Stack } from 'expo-router';
+
+export default function AuthLayout() {
+  return <Stack />
+}
---
+++ src/lib/supabase.ts
@@ -0,0 +1,30 @@
+import 'react-native-url-polyfill/auto';
+import * as SecureStore from 'expo-secure-store';
+import { createClient } from '@supabase/supabase-js';
+import { Database } from '../database.types';
+
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => {
+    return SecureStore.getItemAsync(key);
+  },
+  setItem: (key: string, value: string) => {
+    SecureStore.setItemAsync(key, value);
+  },
+  removeItem: (key: string) => {
+    SecureStore.deleteItemAsync(key);
+  },
+};
+
+const supabaseUrl = 'https://qljrbkwuhkbzcpabkkum.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFsanJia3d1aGtiemNwYWJra3VtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY4MTkxMzMsImV4cCI6MjAzMjM5NTEzM30.Dg086zKPQj83Pxn2qqdF0N0nl74zeC4v31ZMN_1LJ4o'
+
+console.log(supabaseUrl, supabaseAnonKey);
+
+export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: ExpoSecureStoreAdapter as any,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
