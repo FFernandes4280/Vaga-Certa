b214c3763d7397e318849f61ea30fdb0a4d4bee5
FFernandes4280 <d2022002088@unifei.edu.br>
2024-06-11 00:13:22 -0300

    : ) boa noite

--- src/app/(home)/_layout.tsx
+++ src/app/(home)/_layout.tsx
@@ -1,5 +1,16 @@
-import { Stack } from 'expo-router';
+import 'react-native-gesture-handler'
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import Vagas from './vagas';
+
+const Drawer = createDrawerNavigator();
 
 export default function AuthLayout() {
-  return <Stack />
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator>
+        <Drawer.Screen name='Home' component={Vagas}/>
+      </Drawer.Navigator>
+    </NavigationContainer>
+  )
 }
