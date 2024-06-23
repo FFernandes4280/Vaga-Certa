72b4a7045db58c8ba7f11067c36476c2e580c69e
gustavoc5 <gustavorafaelp@hotmail.com>
2024-06-23 12:51:50 -0300

    ..

--- src/app/(home)/_layout.tsx
+++ src/app/(home)/_layout.tsx
@@ -1,27 +1,25 @@
 import 'react-native-gesture-handler';
-import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import Vagas from './vagas';
-import InfoVaga from './info';
-import Mapa from './mapa';
 import ProfileScreen from './perfil';
 import Reservas from './reservas';
+import Anhembi from './Anhembi';
+import Morumbi from './Morumbi';
 
 const Drawer = createDrawerNavigator();
 
-const AuthLayout: React.FC = () => {
+export default function HomeDrawer() {
   return (
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={Vagas} />
-        <Drawer.Screen name="Info" component={InfoVaga} />
-        <Drawer.Screen name="Mapa" component={Mapa} />
-        <Drawer.Screen name="Reservas" component={Reservas} />
-        <Drawer.Screen name="Perfil" component={ProfileScreen} />
-      </Drawer.Navigator>
-  );
-};
-
-export default AuthLayout;
+<Drawer.Navigator>
+  <Drawer.Screen name="Home" component={Vagas} />
+  <Drawer.Screen name="Anhembi" component={Anhembi} options={{ drawerItemStyle: { display: 'none' } }}/> 
+  <Drawer.Screen name="Morumbi" component={Morumbi} options={{ drawerItemStyle: { display: 'none' } }}/>
+  {/* <Drawer.Screen name="Info" component={InfoVaga} options={{ drawerItemStyle: { display: 'none' } }}/> */}
+  <Drawer.Screen name="Reservas" component={Reservas} />
+  <Drawer.Screen name="Perfil" component={ProfileScreen} />
+</Drawer.Navigator>
+) 
+}
 
 // import React from 'react';
 // import TestInfoVaga from './TestInfoVaga';
